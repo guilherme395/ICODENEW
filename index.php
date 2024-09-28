5d5c84f340c9dec8f2165187a3326e053c5df585
Guilherme Lima Leite <guilhermelimaleite06@gmail.com>
2024-09-28 01:06:37 -0300

    Implementado templates para aviso de erros 404 e 500

--- index.php
+++ index.php
@@ -12,26 +12,34 @@ class Router
     public function dispatch()
     {
         $route = trim(str_replace("/ICODENEW", "", parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH)), "/");
+
         if (array_key_exists($route, $this->routes)) {
             require $this->routes[$route];
         } else {
             http_response_code(404);
-            echo "Página não encontrada!";
+            require "Views/Template/Error404.html";
         }
+    }
+
+    public function handle500()
+    {
+        http_response_code(500);
+        require "Views/Template/Error500.html";
     }
 }
 
 $router = new Router();
 
-# Rotas de Views
 $router->add("", "Views/Home.php");
 $router->add("usuario/login", "Views/Login.php");
 $router->add("usuario/cadastrar", "Views/Cadastro.php");
 $router->add("financeiro", "Views/SystemFinance.php");
 
-
-# Rotas de Controllers
 $router->add("usuario/cadastraUsuario", "Controllers/CadastraUsuario.php");
 $router->add("usuario/verificaUsuario", "Controllers/VerificaUsuario.php");
 
-$router->dispatch();
+try {
+    $router->dispatch();
+} catch (Exception $e) {
+    $router->handle500();
+}
